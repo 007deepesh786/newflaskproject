a0a92cc7a71bc13f9f4e19e0cac182a1ed2c76ae
007deepesh786 <pathakdeepesh89@gmail.com>
2024-07-30 12:39:14 +0530

    Revert "hello world undu"
    This reverts commit bceddb7ac57cce8c8581552dad5440e2105ab10f.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/helo')
 def hello_world():  # put application's code here
-    return 'New Hello World!'
+    return 'Hello World!'
 @app.route('/api/products',methods = ['GET'])
 def getAllProducts():  # put application's code here
     return make_response(jsonify({"products": db}), 200)
